d1bf10ed3afed458afd5949021c3f3bdf9f8cd53
driehoeken <a.bednarski1203@gmail.com>
2022-11-16 23:55:52 +0100

    handle cardPortfolio hover

---
+++ main.js
@@ -0,0 +1,27 @@
+const cardPortfolio = document.querySelector(".card-portfolio");
+const cardAboutme = document.querySelector(".card-aboutme");
+const cardContact = document.querySelector(".card-contact");
+const cardPortfolioText = cardPortfolio.querySelector(".card-text");
+const cardAboutmeText = cardAboutme.querySelector(".card-text");
+const cardContactText = cardContact.querySelector(".card-text");
+//cardPortfolio
+cardPortfolio.addEventListener("mouseover", () => {
+    cardPortfolio.style.width = "800px";
+
+    cardAboutme.style.width = "0px";
+    cardAboutme.style.left = "800px";
+
+    cardContact.style.width = "0px";
+    cardContact.style.left = "800px";
+
+    cardAboutmeText.classList.add("card-text-hidden");
+    cardContactText.classList.add("card-text-hidden");
+});
+cardPortfolio.addEventListener("mouseleave", () => {
+    cardPortfolio.style = "";
+    cardAboutme.style = "";
+    cardContact.style = "";
+
+    cardAboutmeText.classList.remove("card-text-hidden");
+    cardContactText.classList.remove("card-text-hidden");
+});
