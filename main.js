1516c60c3b2514ceabdcd03ad4e02074a0032969
driehoeken <a.bednarski1203@gmail.com>
2022-11-16 19:19:41 +0100

    redesigning lmao

--- main.js
+++
@@ -1,9 +0,0 @@
-const sections = document.getElementsByName("section");
-const currSection = 0;
-document.querySelector("body").addEventListener("wheel", (e) => {
-    if (e.deltaY < 0) {
-        console.log("scroll up");
-    } else if (e.deltaY > 0) {
-        console.log("scroll down");
-    }
-});
