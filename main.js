e035454b2f85d67680db4b2b86871d75d6dbbcdd
driehoeken <a.bednarski1203@gmail.com>
2022-11-17 19:58:08 +0100

    card contact hover handling

--- main.js
+++ main.js
@@ -22,10 +22,27 @@ cardAboutme.addEventListener("mouseover", () => {
 
     cardPortfolio.style.width = "0px";
     cardPortfolio.style.height = "0px";
+
     cardContact.style.width = "0px";
     cardContact.style.height = "0px";
 });
 cardAboutme.addEventListener("mouseleave", () => {
+    cardPortfolio.style = "";
+    cardAboutme.style = "";
+    cardContact.style = "";
+});
+//cardContact
+cardContact.addEventListener("mouseover", () => {
+    cardContact.style.width = "800px";
+    cardContact.style.height = "500px";
+
+    cardPortfolio.style.width = "0px";
+    cardPortfolio.style.height = "0px";
+
+    cardAboutme.style.width = "0px";
+    cardAboutme.style.height = "0px";
+});
+cardContact.addEventListener("mouseleave", () => {
     cardPortfolio.style = "";
     cardAboutme.style = "";
     cardContact.style = "";
